0e527be52301cdcac95786663f136454b94c01a4
Administrator <Administrator@192.168.0.103>
2019-01-29 23:08:27 +0800

    二分查找和欧几里得算法

---
+++ src/pra/binarySearch.java
@@ -0,0 +1,34 @@
+package pra;
+
+public class binarySearch {
+	
+	private static final int NOT_FOUND = -1;
+	
+	public static <T extends Comparable<? super T>> 
+	int binarySearch(T[] a,T b) {
+		int low = 0 ,high = a.length-1;
+		
+		while (low <= high) {
+			int mid = (low + high)/2;
+			if (a[mid].compareTo(b)<0) {
+				low = mid + 1;
+			}else if (a[mid].compareTo(b)>0) {
+				high = mid - 1;
+			}else 
+				return mid;
+		}
+		
+		return NOT_FOUND;
+	}
+	
+	public static Long gcd(Long m,Long n) {
+		
+		while (n!=0) {
+			Long rem = m%n;
+			m = n;
+			n = rem;
+		}
+		
+		return m;
+	} 
+}
