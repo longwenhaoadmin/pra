165f4a1f360d2a31d9afbfb80495775174b09498
龙文浩 <1051066383@qq.com>
2019-03-16 11:13:26 +0800

    提交vm注释

--- src/jvm/HeapOOM.java
+++ src/jvm/HeapOOM.java
@@ -7,6 +7,7 @@ import java.util.List;
 *
 *@author longwh
 *@since2019Äê3ÔÂ15ÈÕ
+*vm:-Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -Dfile.encoding=UTF-8
 */
 public class HeapOOM{
 static class OOMObject{
--- src/jvm/YoungGenGC.java
+++ src/jvm/YoungGenGC.java
@@ -3,6 +3,7 @@ package jvm;
 *
 *@author longwh
 *@since2019Äê3ÔÂ15ÈÕ
+*vm:-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
 */
 public class YoungGenGC {
 	public static void main(String[] args) {
